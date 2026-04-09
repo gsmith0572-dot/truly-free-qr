8c489f48eb405829d6deae4e8fe0b2880e3cfa58
George Smith <gsmith0572@gmail.com>
2026-04-08 19:42:03 -0500

    feat: Google AdSense code installed ca-pub-8680464039127093

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,11 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8680464039127093"
+          crossOrigin="anonymous"
+        />
       </head>
       <body className="bg-surface font-inter antialiased">
         {children}
