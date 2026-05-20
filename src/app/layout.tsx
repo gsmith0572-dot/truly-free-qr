751ff96ba706e350cb7ef84c797e4816c9a1a79b
George Smith <gsmith0572@gmail.com>
2026-05-20 13:33:38 -0500

    chore: sync 2026-05-20 13:33

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={inter.className}>
         <Header />
         <main style={{ minHeight: "100vh" }}>{children}</main>
